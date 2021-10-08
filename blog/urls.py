8c66cfca2cfcedd1af1515bfbe6b6148af5e69bb
Matvey <itstoomymail@mail.ru>
2021-10-08 21:27:36 +0500

    Fix some frontend

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('boodoochai.pythonanywhere.com/', views.post_list, name='post_list')
+    path('boodoochai.pythonanywhere.com/', views.post_list, name='post_list'),
+    path('https://boodoochai.pythonanywhere.com/', views.post_list, name='post_list'),
 ]
