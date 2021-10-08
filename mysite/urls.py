0129b980679bb403d96793fa05cc9c2741ad82aa
Matvey <itstoomymail@mail.ru>
2021-10-08 21:20:17 +0500

    Fix some frontend

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('boodoochai.pythonanywhere.com', include('blog.urls')),
+    path('boodoochai.pythonanywhere.com/', include('blog.urls')),
 ]
